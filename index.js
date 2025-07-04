edca6c60bf3267c7986ae5ce88c3348d669324af
rutvikbokadiya <rutvikbokadiya6513@gmail.com>
2025-07-04 19:45:58 +0530

    change score

--- index.js
+++ index.js
@@ -10,8 +10,15 @@ app.get('/', (req, res) => {
   res.send('Hello from Express on port 3000!');
 });
 
-// Start server on port 3000
-const PORT = process.env.PORT || 3005;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.get('/health', (req, res) => {
+  res.send('Hello health Express on port 3005!');
 });
+
+(()=>{
+    // Start server on port 3000
+    const PORT = process.env.PORT || 3005;
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+})()
+
